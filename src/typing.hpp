53bac6e29d85ae6eb4eb86b7fce495400f1d8c52
Qusai Hroub <qusaihroub.r@gmail.com>
2019-07-18 19:50:35 +0300

    Add src/typing.hpp

---
+++ src/typing.hpp
@@ -0,0 +1,40 @@
+#ifndef TYPING_HPP
+#define TYPING_HPP
+
+#include <QObject>
+#include <QDebug>
+#include <QString>
+#include <QTimer>
+#include <QThread>
+
+class Typing: public QObject {
+    Q_OBJECT
+private:
+    QString m_lang;
+    QString m_codeText;
+    QString m_result;
+    int m_triggerCount = 120; // 120 equals to 60 second.
+    QTimer *m_timer = new QTimer();
+    QThread m_qThread;
+    QObject *m_timeLabel;
+
+public:
+    Typing();
+    ~Typing();
+
+public slots:
+    bool save(QString lang, QString codeText);
+    QString getLnag();
+    QString getCodeText();
+    QString getResult();
+    void setTimeLabel(QObject *timeLabel);
+    void startTimer();
+    void endTimer();
+
+private slots:
+
+    // function that executes when the timer ticking
+    void timerSlot();
+};
+
+#endif // TYPING_HPP
