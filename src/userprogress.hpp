dd410c4a9423803e3f517876e7713646dcba5434
Qusai Hroub <qusaihroub.r@gmail.com>
2019-07-20 14:12:31 +0300

    Update userprogress

--- src/userprogress.hpp
+++ src/userprogress.hpp
@@ -16,24 +16,27 @@ class UserProgress : public QObject {
     Q_OBJECT
 private:
     int m_startIndexOfNextWord;
-    int m_NextWordLength;
+    int m_endIndexOfNextWord;
     int m_indexOfFirstMistakeOfUser;
     bool m_userMadeMistake;
+    bool m_endTest;
 
 public:
     explicit UserProgress(QObject *parent = nullptr);
     void setStartIndexOfNextWord(int startIndex);
-    void setNextWordLength(int length);
+    void setEndIndexOfNextWord(int length);
     void setIsUserMadeMistake(bool isMadeMistake);
     void setIndexOfFirstMistakeOfUser(int index);
+    void setIsEndTest(bool endTest);
 
 signals:
 
 public slots:
     int getStartIndexOfNextWord();
-    int getNextWordLength();
+    int getEndIndexOfNextWord();
     bool isUserMadeMistake();
     int getIndexOfFirstMistakeOfUser();
+    bool isEndTest();
 };
 
 #endif // USERPROGRESS_H
